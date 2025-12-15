4a60469c197f7a78f8d977358f9a6fd6349ad7ef
slemanz <william_sleman@hotmail.com>
2025-12-15 04:39:15 -0300

    fourier(feat): add sine cosine dc py

--- python/fourier/sine_cosine.py
+++ python/fourier/sine_cosine.py
@@ -24,3 +24,45 @@ plt.title('Sine and Cosine waves comparison', fontsize=25)
 plt.legend(fontsize=20, loc="upper right")
 plt.xlim([0, 1])
 plt.show()
+
+
+srate = 256  # Hz
+t = np.arange(0., 1., 1/srate) # time vector in seconds
+
+x1 = np.sin(2*np.pi*2*t) #   sinewave
+x2 = 2*np.cos(2*np.pi*4*t) # Cosinewave
+x3 = x1 + x2  # sum of sine and cosine
+
+DC = 2
+x4 = DC + x1 + x2 # sum of sine, cosine and DC.
+
+# Plotting
+plt.figure(figsize=(16,9)) # set the size of figure
+plt.suptitle('Constructing a Wave with Sine, cosine and DC', fontsize=40)
+
+plt.subplot(2, 2, 1) 
+plt.plot(t, x1, linewidth=3) 
+plt.title("Sine Wave of amplitude '1' and frequency '2 Hz'", fontsize=20)
+plt.xlabel('time in sec', fontsize=15)
+plt.ylabel('Amplitude', fontsize=15)
+
+plt.subplot(2, 2, 2)
+plt.plot(t, x2, linewidth=3)
+plt.title("Cosine Wave of amplitude '2' and frequency '4 Hz'", fontsize=20)
+plt.xlabel('time in sec.', fontsize=15)
+plt.ylabel('Amplitude', fontsize=15)
+
+plt.subplot(2, 2, 3)
+plt.plot(t, x3, linewidth=3)
+plt.title("Sum of Sine and Cosine wave", fontsize=20)
+plt.xlabel('time in sec', fontsize=15)
+plt.ylabel('Amplitude', fontsize=15)
+
+plt.subplot(2, 2, 4)
+plt.plot(t, x4, linewidth=3)
+plt.title("Sum of sine, Cosine and DC", fontsize=20)
+plt.xlabel('time in sec', fontsize=15)
+plt.ylabel('Amplitude', fontsize=15)
+
+plt.tight_layout() # prevent overlap
+plt.show()
