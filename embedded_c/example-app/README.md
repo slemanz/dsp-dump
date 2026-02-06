70bc6e9551e2413d343f42bb642d4d6f8bc961fd
slemanz <william_sleman@hotmail.com>
2026-02-06 05:34:24 -0300

    example-app(feat): add plotting in ozone

--- embedded_c/example-app/README.md
+++ embedded_c/example-app/README.md
@@ -1,1 +1,5 @@
 # Example App
+
+- To show the graph in Ozone Segger:
+    1. View -> Data Sampling, and add the global variable to there
+    2. View -> Timeline, and show the variable in running code!
--- embedded_c/example-app/Src/main.c
+++ embedded_c/example-app/Src/main.c
@@ -4,7 +4,18 @@
 #include "driver_systick.h"
 #include "driver_gpio.h"
 
+#include "signals.h"
+
 #define TEST_LENGTH_SAMPLES 4
+
+float g_in_sig_sample; // debug this
+
+
+extern float _5hz_signal[HZ_5_SIG_LEN];
+extern float32_t input_signal_f32_1kHz_15kHz[KHZ1_15_SIG_LEN];
+
+static void plot_input_signal(void);
+static void pseudo_dly(int dly);
 
 int main(void)
 {
@@ -18,15 +29,36 @@ int main(void)
     // Perform optimized addition: dst = srcA + srcB
     arm_add_f32(srcA, srcB, dst, blockSize);
     (void)dst;
+    (void)input_signal_f32_1kHz_15kHz;
     
     uint64_t start_time = ticks_get();
 
     while(1)
     {
+        /*
         if((ticks_get() - start_time) >= 500)
         {
             GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
             start_time = ticks_get();
-        }
+        }*/
+
+        plot_input_signal();
     }
 }
+
+
+static void plot_input_signal(void)
+{
+	int i;
+	for( i = 0; i < HZ_5_SIG_LEN; i++)
+	{
+		g_in_sig_sample = _5hz_signal[i];
+		pseudo_dly(9000);
+	}
+}
+
+static void pseudo_dly(int dly)
+{
+	for( int i = 0; i < dly; i++ ){}
+}
+
