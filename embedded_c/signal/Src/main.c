06eaebe496d2ee841f44c8f33638302141c5a691
slemanz <william_sleman@hotmail.com>
2026-02-09 05:00:53 -0300

    signal(feat): add mean example

--- embedded_c/signal/Src/main.c
+++ embedded_c/signal/Src/main.c
@@ -18,16 +18,39 @@ extern float32_t input_signal_f32_1kHz_15kHz[KHZ1_15_SIG_LEN];
 static void plot_input_signal(void);
 static void pseudo_dly(int dly);
 
+static float32_t signal_mean(float32_t *sig_src_arr, uint32_t sig_length);
+
+float32_t g_mean_value;
+float32_t g_mean_value_arm;
+
 int main(void)
 {
     config_app();
 
     (void)input_signal_f32_1kHz_15kHz;
 
+    g_mean_value = signal_mean((float32_t*)input_signal_f32_1kHz_15kHz, (uint32_t)KHZ1_15_SIG_LEN);
+    arm_mean_f32((float32_t*)input_signal_f32_1kHz_15kHz, (uint32_t)KHZ1_15_SIG_LEN, &g_mean_value_arm);
+
     while(1)
     {
         plot_input_signal();
     }
+}
+
+static float32_t signal_mean(float32_t *sig_src_arr, uint32_t sig_length)
+{
+    float32_t _mean = 0.0;
+    uint32_t i;
+
+    for(i = 0; i < sig_length; i++)
+    {
+        _mean = _mean + sig_src_arr[i];
+    }
+
+    _mean = _mean/(float32_t)sig_length;
+
+    return _mean;
 }
 
 
