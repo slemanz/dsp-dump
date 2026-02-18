2f06c45b5a83936157225c147b6dab7129316968
slemanz <william_sleman@hotmail.com>
2026-02-18 04:52:55 -0300

    superposition(create)

---
+++ embedded_c/superposition/Inc/signals.h
@@ -0,0 +1,10 @@
+#ifndef INC_SIGNALS_H_
+#define INC_SIGNALS_H_
+
+#include <stdint.h>
+#include "arm_math.h"
+
+#define HZ_5_SIG_LEN		301
+#define KHZ1_15_SIG_LEN		320
+
+#endif /* INC_SIGNALS_H_ */
